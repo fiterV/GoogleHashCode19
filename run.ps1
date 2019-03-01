089eae70f023b5eb18e2936a255384e8ae3114ea
Vadym Prokopets <none>
2019-03-01 10:18:21 +0100

    Running script updated

--- run.ps1
+++ run.ps1
@@ -1,6 +1,6 @@
-﻿g++ *.cpp --std=c++11 -o b
-
-start b.exe "tests/e.txt tests/e.out"
-
-
-#start a.exe input2.txt output2.txt
+﻿g++ *.cpp --std=c++11 -o compiled
+start compiled.exe "tests/a.txt tests/a.out"
+start compiled.exe "tests/b.txt tests/b.out"
+start compiled.exe "tests/c.txt tests/c.out"
+start compiled.exe "tests/d.txt tests/d.out"
+start compiled.exe "tests/e.txt tests/e.out"
