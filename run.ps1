2506721f72ab6ce1c4084ba7138952312e44f289
Vadym Prokopets <none>
2019-03-01 12:44:01 +0100

    Script updated

--- run.ps1
+++ run.ps1
@@ -1,6 +1,14 @@
 ﻿g++ *.cpp --std=c++11 -o compiled
-start compiled.exe "tests/a.txt tests/a.out"
-start compiled.exe "tests/b.txt tests/b.out"
-start compiled.exe "tests/c.txt tests/c.out"
-start compiled.exe "tests/d.txt tests/d.out"
-start compiled.exe "tests/e.txt tests/e.out"
+Get-ChildItem "tests/" -Filter *.txt | 
+Foreach-Object {
+    $input  = $_.Name
+    $output = $_.BaseName + ".out"
+    
+    start compiled.exe "tests/$input tests/$output"
+
+}
+#start compiled.exe "tests/a.txt tests/a.out"
+#start compiled.exe "tests/b.txt tests/b.out"
+#start compiled.exe "tests/c.txt tests/c.out"
+#start compiled.exe "tests/d.txt tests/d.out"
+#start compiled.exe "tests/e.txt tests/e.out"
