44ebf9733453c15079cf946fa556f23eb9329778
Vadym Prokopets <none>
2019-03-01 10:15:01 +0100

    Initial template

---
+++ main.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void read_data(){
+
+}
+
+int main(int argc, char *argv[])
+{
+    if (argc == 3){
+        freopen(argv[1], "r", stdin);
+        freopen(argv[2], "w", stdout);
+    } else
+    {
+        freopen("input.txt", "r", stdin);
+    }
+    read_data();
+
+    return 0;
+}
---
+++ run.ps1
@@ -0,0 +1,6 @@
+﻿g++ *.cpp --std=c++11 -o b
+
+start b.exe "tests/e.txt tests/e.out"
+
+
+#start a.exe input2.txt output2.txt
